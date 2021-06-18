e2ebc98fa0aaec0640dd728abc76405bc51e71ea
colyud <33720136+colyud@users.noreply.github.com>
2021-06-18 15:05:23 -0500

    create and styling jumbotron, footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Title, Break, Row, Column, Text, Link } from "./styles/footer";
+
+export default function Footer({ children, ...restProps }) {
+    return <Container {...restProps}> {children}</Container>;
+}
+
+Footer.Title = function FooterTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+};
+
+Footer.Break = function FooterBreak({ children, ...restProps }) {
+    return <Break {...restProps}>{children}</Break>;
+};
+
+Footer.Link = function FooterLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
+};
+
+Footer.Row = function FooterRow({ children, ...restProps }) {
+    return <Row {...restProps}>{children}</Row>;
+};
+
+Footer.Column = function FooterColumn({ children, ...restProps }) {
+    return <Column {...restProps}>{children}</Column>;
+};
+
+Footer.Text = function FooterText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+};
