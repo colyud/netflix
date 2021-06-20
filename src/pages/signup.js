fd346c68d1f7f2118eaa2f055b5be9f3fcb552e1
colyud <33720136+colyud@users.noreply.github.com>
2021-06-20 18:28:54 -0500

    create signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
-import { Form } from "../components";
-import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
-export default function Signin() {
-    const [error, setError] = useState("");
+export default function Signup() {
+    const [firstName, setFirstName] = useState("");
     const [emailAddress, setEmailAddress] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-    const isValid = emailAddress === "" || password === "";
+    const isInvalid = firstName === "" || password === "" || emailAddress === "";
 
-    const handleSignin = (event) => {
+    const handleSignup = (event) => {
         event.preventDefault();
-
-        // call in here to firebase to authenticate the user
-        // if there's an error, populate the error state
     };
 
     return (
         <>
             <HeaderContainer>
                 <Form>
-                    <Form.Title>Sign In</Form.Title>
+                    <Form.Title>Sign Up</Form.Title>
                     {error && <Form.Error>{error}</Form.Error>}
 
-                    <Form.Base onSubmit={handleSignin} method="POST">
-                        <Form.Input placeholder="Email address" value={emailAddress} onChange={({ target }) => setEmailAddress(target.value)} />
+                    <Form.Base onSubmit={handleSignup} method="POST">
+                        <Form.Input placeholder="First Name" value={firstName} onChange={({ target }) => setFirstName(target.value)} />
+                        <Form.Input placeholder="Email Address" value={emailAddress} onChange={({ target }) => setEmailAddress(target.value)} />
                         <Form.Input
                             type="password"
                             value={password}
@@ -33,12 +33,12 @@ export default function Signin() {
                             placeholder="Password"
                             onChange={({ target }) => setPassword(target.value)}
                         />
-                        <Form.Submit disabled={isValid} type="submit">
-                            Sign In
+                        <Form.Submit disabled={isInvalid} type="submit">
+                            Sign Up
                         </Form.Submit>
 
                         <Form.Text>
-                            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                            Already a user? <Form.Link to="/signin">Sign up now.</Form.Link>
                         </Form.Text>
                         <Form.TextSmall>This page is protected by Google reCAPTCHA.</Form.TextSmall>
                     </Form.Base>
