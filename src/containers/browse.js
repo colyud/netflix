3d954ff2331d5382a1b0baae94eb35ea135dc569
colyud <33720136+colyud@users.noreply.github.com>
2021-06-21 16:57:24 -0500

    add search and dropdown (signout) to header in browse

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Header } from "../components";
 import * as ROUTES from "../constants/routes";
 import { FirebaseContext } from "../context/firebase";
@@ -6,16 +6,58 @@ import { SelectProfileContainer } from "./profiles";
 import { FooterContainer } from "./footer";
 
 export function BrowseContainer() {
+    const [category, setCategory] = useState("series");
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const { firebase } = useContext(FirebaseContext);
 
     const user = {
-        displayName: "Loc",
+        displayName: "Karl",
         photoURL: "1",
     };
 
     return profile.displayName ? (
         <>
-            <p>Browse Container</p>
+            <Header src="joker1" dontShowOnSmallViewPort>
+                <Header.Frame>
+                    <Header.Group>
+                        <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix" />
+                        <Header.Link active={category === "series" ? "true" : "false"} onClick={() => setCategory("series")}>
+                            Series
+                        </Header.Link>
+                        <Header.Link active={category === "films" ? "true" : "false"} onClick={() => setCategory("films")}>
+                            Films
+                        </Header.Link>
+                    </Header.Group>
+                    <Header.Group>
+                        <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                        <Header.Profile>
+                            <Header.Picture src={user.photoURL} />
+                            <Header.Dropdown>
+                                <Header.Group>
+                                    <Header.Picture src={user.photoURL} />
+                                    <Header.Link>{user.displayName}</Header.Link>
+                                </Header.Group>
+                                <Header.Group>
+                                    <Header.Link onClick={() => firebase.auth().signOut()}>Sign out</Header.Link>
+                                </Header.Group>
+                            </Header.Dropdown>
+                        </Header.Profile>
+                    </Header.Group>
+                </Header.Frame>
+
+                <Header.Feature>
+                    <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+                    <Header.Text>
+                        Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears
+                        two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's
+                        part of the world around him.
+                    </Header.Text>
+                    <Header.PlayButton>Play</Header.PlayButton>
+                </Header.Feature>
+            </Header>
             <FooterContainer />
         </>
     ) : (
