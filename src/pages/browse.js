900b429686f82f28e146ce1965a6f08061a673b0
colyud <33720136+colyud@users.noreply.github.com>
2021-06-28 15:37:34 -0500

    add live search functional

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -7,11 +7,7 @@ export default function Browse() {
     const { series } = useContent("series");
     const { films } = useContent("films");
 
-    console.log(series);
-    console.log(films);
-
     const slides = selectionMap({ series, films });
 
-    console.log(slides);
     return <BrowseContainer slides={slides} />;
 }
